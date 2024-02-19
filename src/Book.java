6cb8d257311eaa5f8eb63f798de96990198f9f2f
Erik Kovach <kovacherik@gmail.com>
2024-02-18 22:08:59 -0300

    Primer commit

---
+++ src/Book.java
@@ -0,0 +1,59 @@
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private int pages;
+
+
+    // Constructor
+    public Book(String title, String author, int pages, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+
+    // Getter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    // Setter methods (if needed)
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+
+
+    public String toString() {
+        return "Book:" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages='" + pages + '\'' +
+                ", publicationYear=" + publicationYear;
+    }
+}
---
+++ src/Bookstore.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Bookstore {
+    private  Book[] books;
+    private int numberOfBooks;
+
+    // Constructor
+    public Bookstore() {
+        this.books = new Book[10];
+        this.numberOfBooks = 0;
+    }
+
+    // Getter methods
+    public Book[] getBooks() {
+        return this.books;
+    }
+
+    public void addBook(Book newBook) {
+        if (numberOfBooks == books.length) {
+            int newCapacity = books.length * 2;
+            books = Arrays.copyOf(books, newCapacity);
+        }
+
+        books[numberOfBooks] = newBook;
+        numberOfBooks++;
+    }
+
+    public Book[] booksByAuthor(String author) {
+        int count = 0;
+        for (Book book : books) {
+            if (book != null && book.getAuthor().equals(author)) {
+                count++;
+            }
+        }
+
+        Book[] booksByAuthor = new Book[count];
+        int i = 0;
+        for (Book book : books) {
+            if (book != null && book.getAuthor().equals(author)) {
+                booksByAuthor[i] = book;
+                i++;
+            }
+        }
+        return booksByAuthor;
+    }
+
+    public Book[] shortBooks() {
+        int pages = 100;
+        int count = 0;
+        for (Book book : books) {
+            if (book != null && book.getPages() < 100) {
+                count++;
+            }
+        }
+
+        Book[] shortBooks = new Book[count];
+        int i = 0;
+        for (Book book : books) {
+            if (book != null && book.getPages() < 100) {
+                shortBooks[i] = book;
+                i++;
+            }
+        }
+        return shortBooks;
+    }
+
+}
